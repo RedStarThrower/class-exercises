1fe4e521e1ee0704e348c96887580a314e7a3588
Natalia Podosinnikova <Natalia@Natalias-MacBook-Air.local>
2016-03-10 12:28:24 -0600

    countdown-puzzle added

---
+++ countdown-puzzle.js
@@ -0,0 +1,18 @@
+// Write a countdown function that takes a number and counts down to 0
+
+var countdown = function (startingNumber) {
+	for (var i = startingNumber; i >=0; i-- ) {
+		return i
+	}
+}
+
+//console.log(countdown(12)) // logs: 12, 11 ... 0
+
+// Write a function countdown2, that replicates this behavior without using any loop
+
+var countdown2 = function (startingNumber) {	
+	console.log(startingNumber)
+	if (startingNumber >= 0) countdown2(startingNumber - 1)
+}
+
+countdown2(5) // logs 5, 4, 3, 2, 1, 0...
