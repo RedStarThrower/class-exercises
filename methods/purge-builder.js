77f7e09b57d313119d8fb1e8b16c1be64ac9da3d
Natalia Podosinnikova <Natalia@Natalias-MacBook-Air.local>
2016-03-24 10:25:08 -0500

    notes added for purge-builder

--- methods/purge-builder.js
+++ methods/purge-builder.js
@@ -1,4 +1,4 @@
-// Write a method on the array prototype called .purge(). Purge will return a new array that has filtered the elements of the original array. It will take one input, a function that will serve as a rule for inclusion in the new array. 
+// Write a method on the array prototype called .purge(). Purge, like the in-built method .filter() will return a new array that has filtered the elements of the original array. It will take one input, a function that will serve as a rule for inclusion in the new array. 
 
 var numbers = [1, 2, 5, 12, 17, 36]
 
@@ -11,11 +11,14 @@ var isBig = function(num) {
 		else return false
 }
 
-Array.prototype.purge = function(cb) {
+//PART I. WITH A "FOR" LOOP
+
+Array.prototype.purge = function(cb) { //<= Array is the constructor function for all arrays
 	var newArray = []
-	for (var i = 0; i < this.length; i++) {
+	for (var i = 0; i < this.length; i++) { //<= "this" refers to an instance of an array create from the Array constructor
 		var element = this[i]
-		if (cb(element) === true) {
+		//console.log(cb(element)) //<= test a function to see that it logs boolean statements
+		if (cb(element)) { //<= evaluates as "true", so no need to write "if (cb(element)) === true)"
 			newArray.push(element)
 		}
 	}
@@ -27,5 +30,15 @@ var evens = numbers.purge(isEven)
 
 var biguns = numbers.purge(isBig)
 //console.log(biguns) // logs: [12, 17, 36]
+
+//PART II. WITH forEach
+
+Array.prototype.purge = function(cb) {
+	var newArray = []
+	this.forEach(function(el){ //<= forEach is written to transform each element in the array, but give no return value. To create a new array, create an anonymous function that pushes the transformed values into the array. 
+		if (cb(el)) newArray.push(el) //<= don't need brackets for "if" or "else" when there is only one line to execute
+	})
+	return newArray
+}
 
 
